1ff2031c040ef7b8a21fd46fd60ae97beed592d2
OriolLopezMassaguer <oriol.lopez@gmail.com>
2017-07-25 08:56:59 +0200

    individual script test

---
+++ test/test_ws_single_molecule.sh
@@ -0,0 +1,3 @@
+export HOME=/opt/PaDEL-descriptor-ws
+curl "http://localhost:9000/computedescriptors?params=-2d|-3d|-dir|$HOME/input/individual/mol1|-file|$HOME/output/descriptors_ws_individual.txt"
+
